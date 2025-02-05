6d54e5a1d4db588a71382440b46c2f33c66d9db4
Shyam Tala <shyamtala003@gmail.com>
2025-02-05 18:21:10 +0530

    update prettier setting for tesing🙃 just added singleQuote inside .prettierrc

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from "express";
-import { PORT } from "./configs/environment.js";
-import logger from "./utils/logger.js";
-import cors from "./middleware/cors.js";
-import requestLogger from "./middleware/requestLogger.js";
+import express from 'express';
+import { PORT } from './configs/environment.js';
+import logger from './utils/logger.js';
+import cors from './middleware/cors.js';
+import requestLogger from './middleware/requestLogger.js';
 
 const app = express();
 
 app.use(requestLogger());
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.listen(PORT, () => {
   logger.log(`🎸 server is listening on port ${PORT} 🚀`);
