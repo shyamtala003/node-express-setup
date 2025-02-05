68f4bd9e355dd449f4a40272d4122346d11dbc96
Shyam Tala <shyamtala003@gmail.com>
2025-02-05 12:29:53 +0530

    update default accept limit of json and form data

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import cors from "./middleware/cors.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.listen(PORT, () => {
   logger.log(`🎸 server is listening on port ${PORT} 🚀`);
