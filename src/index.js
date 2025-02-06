67ab9a56d7bbdec5d6e2280912d3e943d7267ea5
Shyam Tala <shyamtala003@gmail.com>
2025-02-06 14:58:18 +0530

    move index.js file inside src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { PORT } from './src/configs/environment.js';
-import cors from './src/middleware/cors.js';
-import requestLogger from './src/middleware/requestLogger.js';
-import logger from './src/utils/logger.js';
+import { PORT } from './configs/environment.js';
+import cors from './middleware/cors.js';
+import requestLogger from './middleware/requestLogger.js';
+import logger from './utils/logger.js';
 
 const app = express();
 
@@ -10,6 +10,10 @@ app.use(requestLogger());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello', success: true });
+});
 
 app.listen(PORT, () => {
   logger.log(`🎸 server is listening on port ${PORT} 🚀`);
