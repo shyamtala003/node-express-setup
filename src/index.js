49eecffe9e33ee251d31b3dd411e8a6d9a8d06ab
Shyam Tala <shyamtala003@gmail.com>
2025-02-14 18:08:16 +0530

    🔖I went to track my all logs inside app.log file🙃

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-logger.setConfig({ enableFileLogging: false });
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello', success: true });
 });
